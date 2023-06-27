4cf07537de683aa1fd64ba7a15e915be9675437a
Bogdan Krunic <bogdankrunic@gmail.com>
2023-06-27 12:51:00 +0200

    renamed navigation name

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Frizerski Salon Sita
+            Frizerski Studio Sita
           </a>{" "}
         </div>
 
