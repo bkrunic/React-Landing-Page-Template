86b2cdecb4bd1586746dcc2b1781818671657afd
Bogdan Krunic <bogdankrunic@gmail.com>
2023-06-27 13:22:44 +0200

    contact form update & typo

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -85,7 +85,7 @@ export const Contact = (props) => {
                 </div>
                 <div id="success"></div>
                 <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
+                  Pošalji upit
                 </button>
               </form>
             </div>
